7aabfb6c65fde8eda47ad7e32b516039b9b85538
yedduprudvi@outlook.com <yedduprudvi@outlook.com>
2023-03-14 13:46:24 +0530

    Add new code

--- EntityFrameworkCoreExample/Controllers/CategoryController.cs
+++ EntityFrameworkCoreExample/Controllers/CategoryController.cs
@@ -11,15 +11,125 @@ namespace EntityFrameworkCoreExample.Controllers
         {
             _databaseContext = databaseContext;
         }
-        public IActionResult Add()
+        public IActionResult Index()
         {
-            var category = new Category
-            {
-                Name = "category"
-            };
-            _databaseContext.Category.Add(category);
-            _databaseContext.SaveChanges(); //For reflectioin change to the database
+            IEnumerable<Category> objectCategory = _databaseContext.Category;
+            return View(objectCategory);
+        }
+        
+        //Get Create
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //Post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    _databaseContext.Category.Add(obj);
+                    _databaseContext.SaveChanges(); //For reflectioin change to the database
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            return View(obj);
+        }
+
+        //Get Edit
+        public IActionResult Update(int? id) 
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _databaseContext.Category.Find(id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Category obj)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _databaseContext.Category.Update(obj);
+                    _databaseContext.SaveChanges(); //For reflectioin change to the database
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            return View();
+        }
+
+        public IActionResult GetById(int id)
+        {
+            var category = _databaseContext.Category.Find(id);
+            return Json(category);
+        }
+
+        public IActionResult GetAll(int id)
+        {
+            var categories = _databaseContext.Category.ToList();
+            return Json(categories);
+        }
+
+        //Get Delete
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _databaseContext.Category.Find(id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id)
+        {
+            try
+            {
+                var category = _databaseContext.Category.Find(id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    _databaseContext.Category.Remove(category);
+                    _databaseContext.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- EntityFrameworkCoreExample/Program.cs
+++ EntityFrameworkCoreExample/Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Category}/{action=Add}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
